847422ad25976c69aab535218a53cac58318621f
justinyan13 <justinyan13@gmail.com>
2023-01-21 23:52:55 -0500

    recented on ivey & added bill's sona spot

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var map = L.map('map', {
-  center: [43.64701, -79.39425],
+  center: [43.00440161562366, -81.27666879177272],
   zoom: 15,
 });
 
@@ -15,13 +15,13 @@ var poopIcon = L.icon({
 });
 
 var customPopup =
-  "Great Pop!<br><img src='https://joshuafrazier.info/images/maptime.gif' alt='maptime logo gif' width='350px'/>";
+  "Bill's Favorite Sona!<br><img src='https://media3.giphy.com/media/17C41UvchStTkd92UQ/giphy.gif?cid=790b76113bd14c764941c46c1d72d451d49057dc8e5bdce3&rid=giphy.gif&ct=g' alt='maptime logo gif' width='350px'/>";
 
 var customOptions = {
   maxWidth: '500',
   className: 'custom',
 };
 
-var marker = L.marker([43.64701, -79.39425], { icon: poopIcon })
+var marker = L.marker([43.004566832424494, -81.27834767566313], { icon: poopIcon })
   .bindPopup(customPopup, customOptions)
   .addTo(map);
