2942fde718625211511743047717f4fdf440a1c2
justinyan13 <justinyan13@gmail.com>
2023-01-21 23:39:22 -0500

    poop icon

--- script.js
+++ script.js
@@ -7,4 +7,11 @@ var map = L.map('map',{
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
+  var poopIcon = L.icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/4951/4951989.png',
+    iconSize: [38, 95], // size of the icon
+    });
+
+var marker = L.marker([43.64701, -79.39425], {icon: poopIcon}).addTo(map);
+
 
